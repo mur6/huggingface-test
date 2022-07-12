59a26217a1ed203bd152dc464ab9bc824accf60e
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-12 15:12:17 +0900

    fix

--- infer.py
+++ infer.py
@@ -1,10 +1,12 @@
-from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
-import torch
-
 # import cv2
+import sys
 from pathlib import Path
-from PIL import Image
+
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from PIL import Image
+from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
 
 
 def iter_pil_images():
@@ -23,7 +25,10 @@ images = image_list
 # feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/mit-b0")
 feature_extractor_inference = SegformerFeatureExtractor(do_random_crop=False, do_pad=False)
 # model = SegformerForImageClassification.from_pretrained("nvidia/mit-b0")
-model = SegformerForSemanticSegmentation.from_pretrained("./models")
+
+model_dir = sys.argv[1]
+save_file_name = sys.argv[2]
+model = SegformerForSemanticSegmentation.from_pretrained(model_dir)
 model.eval()
 inputs = feature_extractor_inference(images=images, return_tensors="pt")
 outputs = model(**inputs)
@@ -34,36 +39,4 @@ logits = outputs.logits
 print(f"outputs: {type(outputs)}")
 print(f"outputs.logis: {logits.shape}")
 
-
-import matplotlib.pyplot as plt
-
-
-# fig, axs = plt.subplots(1, 2, figsize=(20, 10))
-# axs[0].imshow(img)
-# axs[1].imshow(color_seg)
-# plt.show()
-
-# fig = plt.figure(tight_layout=True)
-# axes = fig.subplots(1, 2)
-
-num = len(image_list)
-fig, axes = plt.subplots(num, 2)
-# f = plt.figure()
-softmax = torch.nn.Softmax()
-converted = softmax(logits)
-
-# select a sample from the batch
-for index, (orig, result_image) in enumerate(zip(image_list, converted)):
-    # permute to match the desired memory format
-    # result_image = result_image.permute(1, 2, 0).detach().numpy()
-    result_image = result_image.detach().numpy()
-    # plt.imshow(result_image[1])
-    # plt.imshow(result_image[2])
-    # plt.show()
-    # values = np.asarray(result_image, dtype=int)
-    # values = np.unique(values)
-    # print(result_image.shape)
-    # print(values)
-    axes[index, 0].imshow(result_image[1])
-    axes[index, 1].imshow(result_image[2])
-plt.show()
+torch.save(logits, save_file_name)
