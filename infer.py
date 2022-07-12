d4d65e2f3d2025c09411b02322b95b3d5795e113
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-12 15:13:43 +0900

    fix

--- infer.py
+++ infer.py
@@ -13,7 +13,7 @@ def iter_pil_images():
     # samples_dir = sys.argv[1]
     samples_dir = "/Users/taichi.muraki/workspace/machine-learning/mur6-lightning-flash-test/data/samples"
     samples_dir = Path(samples_dir)
-    sample_images = list(samples_dir.glob("*.jpeg"))
+    sample_images = sorted(list(samples_dir.glob("*.jpeg")))
     for p in sample_images:
         image = Image.open(p)
         yield image
